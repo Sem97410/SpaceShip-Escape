c39d7b13d8459a4834374ddda8dc34837a146cca
Damien LEBON <damien.lebon344@gmail.com>
2021-06-25 18:20:32 +0200

    Background + position ship startGame

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx= canvas.getContext('2d');
 const img = new Image();
-img.src = './media/flappy-bird-set.png';
+img.src = './media/spaceShip-escape-set.png';
 
 //general settings
 let gamePlaying = false;        //toggle : je joue ou non ?
 const gravity = .5;
 const speed = 6.2;
-const size = [51,36];           //Taille de l'oiseau (utilisé dans drawImage())
+const size = [33,49];           //Taille de l'oiseau (utilisé dans drawImage())
 const jump = -11.5;
-const cTenth = (canvas.width / 10);
+const cTenth = (canvas.height / 4);
 
 // pipe settings
 const pipeWidth = 78;           //largeur poteau
@@ -39,22 +39,22 @@ let render = () => {
     index ++;
 
     //backGround
-    ctx.drawImage(img, 0,0, canvas.width, canvas.height, -((index *(speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);  
-    ctx.drawImage(img, 0,0, canvas.width, canvas.height, -((index *(speed / 2)) % canvas.width), 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0,0, canvas.width, canvas.height, 0, ((index *(speed)) % canvas.height) , canvas.width, canvas.height);   
+    ctx.drawImage(img, 0,0, canvas.width, canvas.height, 0, ((index *(speed )) % canvas.height) - canvas.height, canvas.width, canvas.height);
 
     //Explication le drawImage va récupérer l'image de fond et la coller sur le canvas. Le modulo permettra de répéter la manoeuvre a chaque fois
     // et le fais de doubler le drawImage tout en enlevant le paramettre "+canvas.width" va permettre de créer un decallage dans le déplacement 
     //ce qui donnera l'impression que c'est continue alors que c'est deux images qui se suivent petit à petit
 
     if (gamePlaying){
-        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size); 
+        ctx.drawImage(img, 432, 0, ...size, cTenth, flyHeight, ...size); 
                                                 //placer l'oiseau a gauche de l'ecran pour débuter le jeux
         flight += gravity;
-        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]); 
+        flyHeight = (canvas.height - size[1] - 30); 
                                                 //l'ajouter du canvas.height ... permet en gros de bloquer l'oiseau en bas de l'écran
 
     }else{
-    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2 ) - size[0] / 2), flyHeight,...size);                            
+    ctx.drawImage(img, 434, 0, ...size, ((canvas.width / 2 ) - size[0] / 2), flyHeight,...size);                            
                                                 //C'est lui qui va copier l'image original et l'ajouter sur le jeux                                           
     flyHeight = (canvas.height / 2) - (size[1] / 2);
 
@@ -64,39 +64,44 @@ let render = () => {
     ctx.font = "bold 30px courier";
     }
 
+            //////////////////////////////////  PIPE GENERATION  //////////////////////////////////////////////////
     //pipe display
-    if(gamePlaying){
-        pipes.map(pipe => {
-            pipe[0] -= speed;
+    // if(gamePlaying){
+    //     pipes.map(pipe => {
+    //         pipe[0] -= speed;
 
-            //top pipe
-            ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+    //         //top pipe
+    //         ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
 
-            //botom pipe
-            ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap );
+    //         //botom pipe
+    //         ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap );
 
-            if (pipe[0] <= -pipeWidth){
-                currentScore ++;
-                bestScore = Math.max (bestScore, currentScore);
+    //         if (pipe[0] <= -pipeWidth){
+    //             currentScore ++;
+    //             bestScore = Math.max (bestScore, currentScore);
 
-                //remove pipe + new pipe
-                pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc() ]];
-                                //Quand un poteau sort a gauche on en regenere un autre a droite
-            }
+    //             //remove pipe + new pipe
+    //             pipes = [...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc() ]];
+    //                             //Quand un poteau sort a gauche on en regenere un autre a droite
+    //         }
+            //////////////////////////////////  PIPE GENERATION  //////////////////////////////////////////////////
+            //////////////////////////////////  END GAME  //////////////////////////////////////////////////
             // if hit the pipe, end
-            if([
-                pipe[0] <= cTenth + size[0],
-                pipe[0] + pipeWidth >= cTenth, //SI l'oiseau se situe au niveau (Y) d'un poteau
-                pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1] //On verifie si il est bien entre les deux
-            ].every (elem => elem)){        //Si il est pas entre les deux poteaux alors on arrete le jeux  et on relance
+            // if([
+            //     pipe[0] <= cTenth + size[0],
+            //     pipe[0] + pipeWidth >= cTenth, //SI l'oiseau se situe au niveau (Y) d'un poteau
+            //     pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1] //On verifie si il est bien entre les deux
+            // ].every (elem => elem)){        //Si il est pas entre les deux poteaux alors on arrete le jeux  et on relance
 
-                gamePlaying = false;
-                setup();
-            }
-        })
+            //     gamePlaying = false;
+            //     setup();
+            // }
+
+           
+    //     })
         
-    }
-
+    // }
+ //////////////////////////////////  END GAME  //////////////////////////////////////////////////
     
     document.getElementById('bestScore').innerHTML = `Meilleur : ${bestScore}`;
     document.getElementById('currentScore').innerHTML = `Actuel : ${currentScore}`;
